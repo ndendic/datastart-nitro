2493f49dbafef31db9f4892f22334d60964dd742
legovh <legovh>
2025-12-08 15:20:10 +0100

    dev

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import asyncio
+import logging
+from uuid import uuid4
 from datetime import datetime
 
 from sanic import Sanic
@@ -10,6 +12,14 @@ app = Sanic(__name__)
 app.static('/static/', './static/')
 app.static('/', './index.html', name="index")
 
+logging.basicConfig(filename='perso.log', encoding='utf-8', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+@app.on_response
+async def cookie(request, response):
+    if not request.cookies.get("user_id"):
+        user_id = uuid4().hex
+        response.add_cookie('user_id', user_id)
 
 @app.get("/load")
 @datastar_response
@@ -20,4 +30,11 @@ async def load(request):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, auto_reload=True, access_log=False)
+    # while developing
+    # app.run(debug=True, auto_reload=True, access_log=False)
+    # else
+    app.run(
+    debug=False,
+    auto_reload=True,
+    unix="your_unix_sock_here"
+    access_log=False)
